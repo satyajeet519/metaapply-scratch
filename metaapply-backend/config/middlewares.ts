2bd31c65e1e58e0079894727ff4776a3e2e5e8f2
Ekta Gupta <ektaguptaa858@gmail.com>
2026-03-09 15:28:52 +0530

    Convert backend from submodule to normal folder

---
+++ metaapply-backend/config/middlewares.ts
@@ -0,0 +1,23 @@
+import type { Core } from '@strapi/strapi';
+
+const config: Core.Config.Middlewares = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: '*',
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
+export default config;
