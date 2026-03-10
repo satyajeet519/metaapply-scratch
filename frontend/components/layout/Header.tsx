4316be0623acf38585f94cca1be8db8cc0312709
Ekta Gupta <ektaguptaa858@gmail.com>
2026-03-10 13:33:58 +0530

    API Called in Layout

---
+++ frontend/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header({menus} : any){
+    return(
+        <>
+            <h1>Test</h1>
+        </>
+    )
+}
--- frontend/services/httpServices.ts
+++ frontend/services/httpServices.ts
@@ -1,3 +1,31 @@
 import axios, {AxiosError} from "axios";
 
-// export const getServices = async 
+const API_URL = "http://localhost:1337/api";
+
+export const getServices = async (url: string, params? : any) => {
+    try {
+        const response = await axios.get(`${API_URL}${url}`, { params });
+        
+        return{
+            isSuccess: true,
+            statusCode: response.status,
+            data: response.data,
+            message: null,
+        }
+    } catch (e: unknown) {
+        if(e instanceof AxiosError){
+            return{
+                isSuccess: false,
+                statusCode: e?.response?.status,
+                data: null,
+                message: e?.response?.data?.error,
+            }
+        }
+        return{
+            isSuccess: false,
+            statusCode: 500,
+            data: null,
+            message: "Something went wrong",
+        }
+    }
+}
